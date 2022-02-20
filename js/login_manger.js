0feed70dcc80502a3c3932fe5d49afae17773b06
holasa21 <93394447+holasa21@users.noreply.github.com>
2022-02-20 14:17:40 +0300

    Add files via upload

---
+++ js/login_manger.js
@@ -0,0 +1,29 @@
+function validation(){
+	var input_text = document.querySelector("#input_text");
+	var input_password = document.querySelector("#input_password");
+	var error_msg = document.querySelector(".error_msg");
+
+	if(input_text.value.length <= 0 || input_password.value.length <= 3 ){
+		error_msg.style.display = "inline-block";
+		input_text.style.border = "1px solid #f74040";
+		input_password.style.border = "1px solid #f74040";
+		return false;
+	}
+	else{
+		window.location.href="manger_home_page.html";
+		return true;
+	}
+	
+}
+
+var input_fields = document.querySelectorAll(".input");
+var login_btn = document.querySelector("#login_btn");
+
+input_fields.forEach(function(input_item){
+	input_item.addEventListener("input", function(){
+		if(input_item.value.length > 3){
+			login_btn.disabled = false;
+			login_btn.className = "btn enabled"
+		}
+	})
+})
