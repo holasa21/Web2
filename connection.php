d37487b1ba629272eb744e887abf75f4e6d64e0d
SuperRenad <reryxry@gmail.com>
2022-04-06 03:27:02 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -11,3 +11,5 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+//رابط الموقع
+$sys_url = 'http://127.0.01/web2/';
